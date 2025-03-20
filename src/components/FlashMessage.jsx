cb0292c3e96a12b2875684ac6305ebe6c6006632
Navneet Prajapati <144175174+NavneetPrajapati31@users.noreply.github.com>
2025-03-20 23:20:15 +0530

    Update FlashMessage.jsx

--- src/components/FlashMessage.jsx
+++ src/components/FlashMessage.jsx
@@ -11,15 +11,15 @@ export default function FlashMessage({ message, type = "success", onClose }) {
 
   // Define colors based on message type (success, error, warning, etc.)
   const colors = {
-    success: "border-blue-600",
-    error: "border-red-600",
+    success: "border-blue-700/50 text-blue-700",
+    error: "border-red-700/50 text-red-700",
     warning: "border-yellow-500",
-    info: "border-blue-500",
+    info: "border-blue-700/50",
   };
 
   return (
     <div
-      className={`fixed bottom-5 right-5 border- border-solid border-1 ${colors[type]} text-gray-400 px-6 py-3 rounded-lg text-sm font-semibold animate-fadeInOut`}
+      className={`fixed bottom-5 right-5 border ${colors[type]} px-6 py-3 rounded-lg text-sm font-semibold animate-fadeInOut`}
     >
       {message}
     </div>
